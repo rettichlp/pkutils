b619a3681bf8a663426af9378eee76bfdee326c3
RettichLP <rettichlp01@gmail.com>
2025-08-19 10:54:49 +0200

    Format code for consistency and readability across multiple files

--- src/client/java/de/rettichlp/PKUtilsClient.java
+++ src/client/java/de/rettichlp/PKUtilsClient.java
@@ -3,8 +3,9 @@ package de.rettichlp;
 import net.fabricmc.api.ClientModInitializer;
 
 public class PKUtilsClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
-	}
+
+    @Override
+    public void onInitializeClient() {
+        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+    }
 }
