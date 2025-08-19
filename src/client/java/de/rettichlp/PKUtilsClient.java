1f94b1cb7b312e3e2afa47850d6cafa8f8c5e676
RettichLP <rettichlp01@gmail.com>
2025-08-19 10:50:41 +0200

    Add default project structure

---
+++ src/client/java/de/rettichlp/PKUtilsClient.java
@@ -0,0 +1,10 @@
+package de.rettichlp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PKUtilsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
