4a4f06ca6b29dd2f6eaa2927bdad0b7230123114
RettichLP <rettichlp01@gmail.com>
2025-08-21 19:32:16 +0200

    Add @ToString annotation to FactionMember and WantedEntry classes

--- src/client/java/de/rettichlp/common/storage/schema/FactionMember.java
+++ src/client/java/de/rettichlp/common/storage/schema/FactionMember.java
@@ -2,8 +2,10 @@ package de.rettichlp.common.storage.schema;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 @AllArgsConstructor
 public class FactionMember {
 
