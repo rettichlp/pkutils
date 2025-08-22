59fde0f442531971d8a6c1e761b91dbf75b1bbcc
RettichLP <rettichlp01@gmail.com>
2025-08-22 11:22:37 +0200

    Implement game synchronization process and message handling in SyncManager

--- src/client/java/de/rettichlp/common/manager/BaseManager.java
+++ src/client/java/de/rettichlp/common/manager/BaseManager.java
@@ -1,14 +1,36 @@
 package de.rettichlp.common.manager;
 
 import lombok.NoArgsConstructor;
+import net.minecraft.text.Text;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
+import static de.rettichlp.PKUtilsClient.player;
 import static net.minecraft.client.MinecraftClient.getInstance;
+import static net.minecraft.text.Text.of;
+import static net.minecraft.util.Formatting.DARK_GRAY;
+import static net.minecraft.util.Formatting.DARK_PURPLE;
+import static net.minecraft.util.Formatting.LIGHT_PURPLE;
+import static net.minecraft.util.Formatting.WHITE;
 
 @NoArgsConstructor
 public class BaseManager {
+
+    private static final Text modMessagePrefix = Text.empty()
+            .append(of("✦").copy().formatted(DARK_PURPLE))
+            .append(of(" "))
+            .append(of("PKU").copy().formatted(LIGHT_PURPLE))
+            .append(of(" "))
+            .append(of("|").copy().formatted(DARK_GRAY))
+            .append(of(" "));
+
+    public void sendModMessage(String message, boolean inActionbar) {
+        Text messageText = modMessagePrefix.copy()
+                .append(of(message).copy().formatted(WHITE));
+
+        player.sendMessage(messageText, inActionbar);
+    }
 
     public void delayedAction(Runnable runnable, long milliseconds) {
         new Timer().schedule(new TimerTask() {
