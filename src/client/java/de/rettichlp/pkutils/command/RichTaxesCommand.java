4f16d3d0f5d093bdd40458fdef9122b49aa62ff9
RettichLP <rettichlp01@gmail.com>
2025-08-22 13:26:15 +0200

    refactor: update RichTaxesCommand to extend CommandManager and improve message handling

--- src/client/java/de/rettichlp/pkutils/command/RichTaxesCommand.java
+++ src/client/java/de/rettichlp/pkutils/command/RichTaxesCommand.java
@@ -2,51 +2,55 @@ package de.rettichlp.pkutils.command;
 
 import com.mojang.brigadier.CommandDispatcher;
 import de.rettichlp.pkutils.common.listener.IMessageListener;
-import de.rettichlp.pkutils.common.manager.BaseManager;
+import de.rettichlp.pkutils.common.manager.CommandManager;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.minecraft.text.Text;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static de.rettichlp.pkutils.PKUtilsClient.networkHandler;
-import static de.rettichlp.pkutils.PKUtilsClient.player;
 import static java.util.regex.Pattern.compile;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
-public class RichTaxesCommand extends BaseManager implements IMessageListener {
+public class RichTaxesCommand extends CommandManager implements IMessageListener {
 
-    private final Pattern PLAYER_MONEY_BANK_AMOUNT = compile("^Ihr Bankguthaben beträgt: (?<moneyBankAmount>([+-])\\d+)\\$$");
-    private final Pattern MONEY_ATM_AMOUNT = compile("ATM \\d+: (?<atmMoneyAmount>\\d+)/100000\\$");
+    private static final Pattern PLAYER_MONEY_BANK_AMOUNT = compile("^Ihr Bankguthaben beträgt: (?<moneyBankAmount>([+-])\\d+)\\$$");
+    private static final Pattern MONEY_ATM_AMOUNT = compile("ATM \\d+: (?<moneyAtmAmount>\\d+)/100000\\$");
 
     private int moneyBankAmount = 0;
     private int atmMoneyAmount = 0;
 
-    public void register(CommandDispatcher<FabricClientCommandSource> dispatcher) {
+    @Override
+    public void register(@NotNull CommandDispatcher<FabricClientCommandSource> dispatcher) {
         dispatcher.register(
                 literal("reichensteuer")
                         .executes(context -> {
+                            // execute command to check money on bank of player
                             networkHandler.sendChatCommand("bank info");
 
+                            // execute command to check money in atm
                             delayedAction(() -> {
-                                networkHandler.sendChatCommand("atminfo");
-                                int amount = 0;
-
-                                if (this.moneyBankAmount < 100000) {
-                                    player.sendMessage(Text.of("Du hast nicht genug Geld auf der Bank."), false);
-                                    return;
-                                }
-
-                                if (this.atmMoneyAmount < this.moneyBankAmount) {
-                                    amount = this.moneyBankAmount - this.atmMoneyAmount;
-                                    networkHandler.sendChatCommand("bank abbuchen " + amount);
-                                    player.sendMessage(Text.of("Du musst noch " + amount + "$ abbuchen."), false);
-                                    return;
-                                }
-
-                                amount = this.moneyBankAmount - 100000;
-                                networkHandler.sendChatCommand("bank abbuchen " + amount);
+                                networkHandler.sendChatCommand("atm info");
                             }, 1000);
+
+                            // handle money withdraw
+                            delayedAction(() -> {
+                                // check player has rich taxes
+                                if (this.moneyBankAmount <= 100000) {
+                                    sendModMessage("Du hast nicht ausreichend Geld auf der Bank.", false);
+                                    return;
+                                }
+
+                                int moneyThatNeedsToBeWithdrawn = this.moneyBankAmount - 100000;
+
+                                if (this.atmMoneyAmount >= moneyThatNeedsToBeWithdrawn) {
+                                    networkHandler.sendChatCommand("bank abbuchen " + moneyThatNeedsToBeWithdrawn);
+                                } else {
+                                    networkHandler.sendChatCommand("bank abbuchen " + this.atmMoneyAmount);
+                                    sendModMessage("Du musst noch " + (moneyThatNeedsToBeWithdrawn - this.atmMoneyAmount) + "$ abbuchen.", false);
+                                }
+                            }, 2000);
 
                             return 1;
                         })
@@ -54,26 +58,19 @@ public class RichTaxesCommand extends BaseManager implements IMessageListener {
     }
 
     @Override
-    public void onMessage(String message) {
-        Matcher matcher = PLAYER_MONEY_BANK_AMOUNT.matcher(message);
-        if (matcher.find()) {
-            String moneyBankAmountString = matcher.group("moneyBankAmount");
-            try {
-                this.moneyBankAmount = Integer.parseInt(moneyBankAmountString);
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-            }
-            return;
+    public boolean onMessage(String message) {
+        Matcher playerMoneyBankAmountMatcher = PLAYER_MONEY_BANK_AMOUNT.matcher(message);
+        if (playerMoneyBankAmountMatcher.find()) {
+            this.moneyBankAmount = Integer.parseInt(playerMoneyBankAmountMatcher.group("moneyBankAmount"));
+            return true;
         }
 
-        Matcher atmMatcher = MONEY_ATM_AMOUNT.matcher(message);
-        if (atmMatcher.find()) {
-            String atmMoneyAmountString = atmMatcher.group("atmMoneyAmount");
-            try {
-                this.atmMoneyAmount = Integer.parseInt(atmMoneyAmountString);
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-            }
+        Matcher moneyAtmAmountMatcher = MONEY_ATM_AMOUNT.matcher(message);
+        if (moneyAtmAmountMatcher.find()) {
+            this.atmMoneyAmount = Integer.parseInt(moneyAtmAmountMatcher.group("moneyAtmAmount"));
+            return true;
         }
+
+        return true;
     }
 }
