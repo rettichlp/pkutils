81397a8191638d39976998ec4201becb77749a9e
RettichLP <rettichlp01@gmail.com>
2025-08-25 13:30:30 +0200

    feat: implement listener registration for command and message handling

--- src/client/java/de/rettichlp/pkutils/PKUtilsClient.java
+++ src/client/java/de/rettichlp/pkutils/PKUtilsClient.java
@@ -2,28 +2,18 @@ package de.rettichlp.pkutils;
 
 import de.rettichlp.pkutils.common.listener.impl.faction.BlacklistListener;
 import de.rettichlp.pkutils.common.listener.impl.faction.FactionChatListener;
+import de.rettichlp.pkutils.common.listener.impl.faction.WantedListener;
 import de.rettichlp.pkutils.common.listener.impl.job.FisherListener;
 import de.rettichlp.pkutils.common.listener.impl.job.GarbageManListener;
 import de.rettichlp.pkutils.common.listener.impl.job.TransportListener;
 import de.rettichlp.pkutils.common.manager.SyncManager;
-import de.rettichlp.pkutils.common.listener.impl.faction.WantedListener;
+import de.rettichlp.pkutils.common.registry.Registry;
 import de.rettichlp.pkutils.common.storage.Storage;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
-import net.fabricmc.fabric.api.client.message.v1.ClientSendMessageEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.util.math.BlockPos;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.StringJoiner;
-
-import static de.rettichlp.pkutils.common.registry.Registry.registerCommands;
-import static java.lang.Character.isUpperCase;
-import static java.util.Objects.isNull;
 
 public class PKUtilsClient implements ClientModInitializer {
 
@@ -41,7 +31,7 @@ public class PKUtilsClient implements ClientModInitializer {
     public static SyncManager syncManager;
     public static WantedListener wantedListener;
 
-    private BlockPos lastPlayerPos = null;
+    private final Registry registry = new Registry();
 
     @Override
     public void onInitializeClient() {
@@ -59,83 +49,12 @@ public class PKUtilsClient implements ClientModInitializer {
             assert minecraftClient.player != null; // cannot be null at this point
             player = minecraftClient.player;
             networkHandler = minecraftClient.player.networkHandler;
+
+            this.registry.registerListeners();
         }));
 
-        ClientTickEvents.END_CLIENT_TICK.register((server) -> {
-            // ignore ticks until the player is initialized
-            if (player == null || networkHandler == null) {
-                return;
-            }
-
-            BlockPos blockPos = player.getBlockPos();
-            if (isNull(this.lastPlayerPos) || !this.lastPlayerPos.equals(blockPos)) {
-                this.lastPlayerPos = blockPos;
-                garbageManListener.onMove(blockPos);
-            }
-        });
-
-        ClientReceiveMessageEvents.ALLOW_GAME.register((message, overlay) -> {
-            // ignore messages until the player is initialized
-            if (player == null || networkHandler == null) {
-                return true;
-            }
-
-            String rawMessage = message.getString();
-
-            boolean showMessage1 = blacklistListener.onMessageReceive(rawMessage);
-            boolean showMessage2 = fisherListener.onMessageReceive(rawMessage);
-            boolean showMessage3 = garbageManListener.onMessageReceive(rawMessage);
-            boolean showMessage4 = transportListener.onMessageReceive(rawMessage);
-            boolean showMessage5 = syncManager.onMessageReceive(rawMessage);
-            boolean showMessage6 = wantedListener.onMessageReceive(rawMessage);
-
-            if (rawMessage.equals("Du hast dein Ziel erreicht!")) {
-                fisherListener.onNaviSpotReached();
-                transportListener.onNaviSpotReached();
-            }
-
-            return showMessage1 && showMessage2 && showMessage3 && showMessage4 && showMessage5 && showMessage6;
-        });
-
-        ClientSendMessageEvents.ALLOW_CHAT.register(message -> {
-            boolean sendMessage1 = factionChatListener.onMessageSend(message);
-
-            return sendMessage1;
-        });
-
-        ClientSendMessageEvents.ALLOW_COMMAND.register(command -> {
-            String[] parts = command.split(" ", 2); // split the message into command label and arguments
-            String commandLabel = parts[0]; // get the command label
-
-            if (containsUppercase(commandLabel)) {
-                String labelLowerCase = commandLabel.toLowerCase(); // get the lowercase command label
-
-                StringJoiner stringJoiner = new StringJoiner(" ");
-                stringJoiner.add(labelLowerCase);
-
-                if (parts.length > 1) {
-                    stringJoiner.add(parts[1]);
-                }
-
-                networkHandler.sendChatCommand(stringJoiner.toString());
-                return false;
-            }
-
-            return true;
-        });
-
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
-            registerCommands(dispatcher);
+            this.registry.registerCommands(dispatcher);
         });
-    }
-
-    private boolean containsUppercase(@NotNull String input) {
-        for (char c : input.toCharArray()) {
-            if (isUpperCase(c)) {
-                return true;
-            }
-        }
-
-        return false;
     }
 }
--- src/client/java/de/rettichlp/pkutils/common/registry/Registry.java
+++ src/client/java/de/rettichlp/pkutils/common/registry/Registry.java
@@ -2,19 +2,34 @@ package de.rettichlp.pkutils.common.registry;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import de.rettichlp.pkutils.common.listener.ICommandSendListener;
+import de.rettichlp.pkutils.common.listener.IMessageReceiveListener;
+import de.rettichlp.pkutils.common.listener.IMessageSendListener;
+import de.rettichlp.pkutils.common.listener.IMoveListener;
+import de.rettichlp.pkutils.common.listener.INaviSpotReachedListener;
 import de.rettichlp.pkutils.common.manager.CommandBase;
+import de.rettichlp.pkutils.common.manager.PKUtilsBase;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
+import net.fabricmc.fabric.api.client.message.v1.ClientSendMessageEvents;
+import net.minecraft.util.math.BlockPos;
 import org.atteo.classindex.ClassIndex;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.InvocationTargetException;
 
 import static com.mojang.text2speech.Narrator.LOGGER;
+import static de.rettichlp.pkutils.PKUtilsClient.networkHandler;
+import static de.rettichlp.pkutils.PKUtilsClient.player;
+import static java.util.Objects.isNull;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
 public class Registry {
 
-    public static void registerCommands(@NotNull CommandDispatcher<FabricClientCommandSource> dispatcher) {
+    private BlockPos lastPlayerPos = null;
+
+    public void registerCommands(@NotNull CommandDispatcher<FabricClientCommandSource> dispatcher) {
         for (Class<?> commandClass : ClassIndex.getAnnotated(PKUtilsCommand.class)) {
             try {
                 String label = commandClass.getAnnotation(PKUtilsCommand.class).label();
@@ -26,6 +41,57 @@ public class Registry {
                 dispatcher.register(enrichedNode);
             } catch (InvocationTargetException | InstantiationException | IllegalAccessException | NoSuchMethodException e) {
                 LOGGER.error("Error while registering command: {}", commandClass.getName(), e.getCause());
+            }
+        }
+    }
+
+    public void registerListeners() {
+        // ignore messages until the player is initialized
+        if (player == null || networkHandler == null) {
+            return;
+        }
+
+        for (Class<?> listenerClass : ClassIndex.getAnnotated(PKUtilsListener.class)) {
+            try {
+                PKUtilsBase listenerInstance = (PKUtilsBase) listenerClass.getConstructor().newInstance();
+
+                if (listenerInstance instanceof ICommandSendListener iCommandSendListener) {
+                    ClientSendMessageEvents.ALLOW_COMMAND.register(iCommandSendListener::onCommandSend);
+                }
+
+                if (listenerInstance instanceof IMessageReceiveListener iMessageReceiveListener) {
+                    ClientReceiveMessageEvents.ALLOW_GAME.register((message, overlay) -> {
+                        String rawMessage = message.getString();
+                        return iMessageReceiveListener.onMessageReceive(rawMessage);
+                    });
+                }
+
+                if (listenerInstance instanceof IMessageSendListener iMessageSendListener) {
+                    ClientSendMessageEvents.ALLOW_CHAT.register(iMessageSendListener::onMessageSend);
+                }
+
+                if (listenerInstance instanceof IMoveListener iMoveListener) {
+                    ClientTickEvents.END_CLIENT_TICK.register((server) -> {
+                        BlockPos blockPos = player.getBlockPos();
+                        if (isNull(this.lastPlayerPos) || !this.lastPlayerPos.equals(blockPos)) {
+                            this.lastPlayerPos = blockPos;
+                            iMoveListener.onMove(blockPos);
+                        }
+                    });
+                }
+
+                if (listenerInstance instanceof INaviSpotReachedListener iNaviSpotReachedListener) {
+                    ClientReceiveMessageEvents.ALLOW_GAME.register((message, overlay) -> {
+                        String rawMessage = message.getString();
+                        if (rawMessage.equals("Du hast dein Ziel erreicht!")) {
+                            iNaviSpotReachedListener.onNaviSpotReached();
+                        }
+
+                        return true;
+                    });
+                }
+            } catch (InvocationTargetException | InstantiationException | IllegalAccessException | NoSuchMethodException e) {
+                LOGGER.error("Error while registering listener: {}", listenerClass.getName(), e.getCause());
             }
         }
     }
