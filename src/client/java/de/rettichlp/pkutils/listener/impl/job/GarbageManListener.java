6679c6b9bb087c8eacd4c9c71920d32065d64e79
RettichLP <rettichlp01@gmail.com>
2025-08-25 15:38:38 +0200

    fix: adjust cooldown duration for waste drop command in GarbageManListener

--- src/client/java/de/rettichlp/pkutils/listener/impl/job/GarbageManListener.java
+++ src/client/java/de/rettichlp/pkutils/listener/impl/job/GarbageManListener.java
@@ -89,7 +89,7 @@ public class GarbageManListener extends PKUtilsBase implements IMessageReceiveLi
 
                 networkHandler.sendChatCommand("dropwaste");
             }
-        }, 0, 5000);
+        }, 0, 5200);
     }
 
     private WasteDropSpot getNearestWasteDropSpot() {
