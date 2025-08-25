cb53bc4a72287601e0fc192658cc2fd75dc1da47
RettichLP <rettichlp01@gmail.com>
2025-08-25 12:12:50 +0200

    feat: refactor command classes to extend CommandBase and implement execute method

--- src/client/java/de/rettichlp/pkutils/command/RichTaxesCommand.java
+++ src/client/java/de/rettichlp/pkutils/command/RichTaxesCommand.java
@@ -1,8 +1,9 @@
 package de.rettichlp.pkutils.command;
 
-import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import de.rettichlp.pkutils.common.listener.IMessageReceiveListener;
-import de.rettichlp.pkutils.common.manager.CommandManager;
+import de.rettichlp.pkutils.common.manager.CommandBase;
+import de.rettichlp.pkutils.common.registry.PKUtilsCommand;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import org.jetbrains.annotations.NotNull;
 
@@ -11,9 +12,9 @@ import java.util.regex.Pattern;
 
 import static de.rettichlp.pkutils.PKUtilsClient.networkHandler;
 import static java.util.regex.Pattern.compile;
-import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
-public class RichTaxesCommand extends CommandManager implements IMessageReceiveListener {
+@PKUtilsCommand(label = "reichensteuer")
+public class RichTaxesCommand extends CommandBase implements IMessageReceiveListener {
 
     private static final Pattern PLAYER_MONEY_BANK_AMOUNT = compile("^Ihr Bankguthaben beträgt: (?<moneyBankAmount>([+-])\\d+)\\$$");
     private static final Pattern MONEY_ATM_AMOUNT = compile("ATM \\d+: (?<moneyAtmAmount>\\d+)/100000\\$");
@@ -22,37 +23,35 @@ public class RichTaxesCommand extends CommandManager implements IMessageReceiveL
     private int atmMoneyAmount = 0;
 
     @Override
-    public void register(@NotNull CommandDispatcher<FabricClientCommandSource> dispatcher) {
-        dispatcher.register(
-                literal("reichensteuer")
-                        .executes(context -> {
-                            // execute command to check money on the bank of player
-                            networkHandler.sendChatCommand("bank info");
+    public LiteralArgumentBuilder<FabricClientCommandSource> execute(@NotNull LiteralArgumentBuilder<FabricClientCommandSource> node) {
+        return node
+                .executes(context -> {
+                    // execute command to check money on the bank of player
+                    networkHandler.sendChatCommand("bank info");
 
-                            // execute command to check money in atm
-                            delayedAction(() -> networkHandler.sendChatCommand("atm info"), 1000);
+                    // execute command to check money in atm
+                    delayedAction(() -> networkHandler.sendChatCommand("atm info"), 1000);
 
-                            // handle money withdraw
-                            delayedAction(() -> {
-                                // check player has rich taxes
-                                if (this.moneyBankAmount <= 100000) {
-                                    sendModMessage("Du hast nicht ausreichend Geld auf der Bank.", false);
-                                    return;
-                                }
+                    // handle money withdraw
+                    delayedAction(() -> {
+                        // check player has rich taxes
+                        if (this.moneyBankAmount <= 100000) {
+                            sendModMessage("Du hast nicht ausreichend Geld auf der Bank.", false);
+                            return;
+                        }
 
-                                int moneyThatNeedsToBeWithdrawn = this.moneyBankAmount - 100000;
+                        int moneyThatNeedsToBeWithdrawn = this.moneyBankAmount - 100000;
 
-                                if (this.atmMoneyAmount >= moneyThatNeedsToBeWithdrawn) {
-                                    networkHandler.sendChatCommand("bank abbuchen " + moneyThatNeedsToBeWithdrawn);
-                                } else {
-                                    networkHandler.sendChatCommand("bank abbuchen " + this.atmMoneyAmount);
-                                    sendModMessage("Du musst noch " + (moneyThatNeedsToBeWithdrawn - this.atmMoneyAmount) + "$ abbuchen.", false);
-                                }
-                            }, 2000);
+                        if (this.atmMoneyAmount >= moneyThatNeedsToBeWithdrawn) {
+                            networkHandler.sendChatCommand("bank abbuchen " + moneyThatNeedsToBeWithdrawn);
+                        } else {
+                            networkHandler.sendChatCommand("bank abbuchen " + this.atmMoneyAmount);
+                            sendModMessage("Du musst noch " + (moneyThatNeedsToBeWithdrawn - this.atmMoneyAmount) + "$ abbuchen.", false);
+                        }
+                    }, 2000);
 
-                            return 1;
-                        })
-        );
+                    return 1;
+                });
     }
 
     @Override
