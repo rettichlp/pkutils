9ed93402602c734353ba6dbed75fad07490b7690
RettichLP <rettichlp01@gmail.com>
2025-08-28 21:00:16 +0200

    refactor: streamline ClearActivityCommand by using static imports and simplifying player suggestions

--- src/client/java/de/rettichlp/pkutils/command/ClearActivityCommand.java
+++ src/client/java/de/rettichlp/pkutils/command/ClearActivityCommand.java
@@ -1,22 +1,22 @@
 package de.rettichlp.pkutils.command;
 
-import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.mojang.brigadier.suggestion.Suggestions;
-import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import de.rettichlp.pkutils.common.registry.CommandBase;
 import de.rettichlp.pkutils.common.registry.PKUtilsCommand;
 import de.rettichlp.pkutils.common.storage.schema.Faction;
+import de.rettichlp.pkutils.common.storage.schema.FactionMember;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.minecraft.command.CommandSource;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.concurrent.CompletableFuture;
-
+import static com.mojang.brigadier.arguments.StringArgumentType.getString;
+import static com.mojang.brigadier.arguments.StringArgumentType.word;
+import static com.mojang.brigadier.suggestion.Suggestions.empty;
 import static de.rettichlp.pkutils.PKUtilsClient.activityService;
 import static de.rettichlp.pkutils.PKUtilsClient.player;
 import static de.rettichlp.pkutils.PKUtilsClient.storage;
+import static de.rettichlp.pkutils.common.storage.schema.Faction.NULL;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
+import static net.minecraft.command.CommandSource.suggestMatching;
 
 @PKUtilsCommand(label = "clearactivity")
 public class ClearActivityCommand extends CommandBase {
@@ -24,28 +24,17 @@ public class ClearActivityCommand extends CommandBase {
     @Override
     public LiteralArgumentBuilder<FabricClientCommandSource> execute(@NotNull LiteralArgumentBuilder<FabricClientCommandSource> node) {
         return node
-                .then(argument("targetName", StringArgumentType.word())
-                        .suggests(this::factionMemberSuggestions)
+                .then(argument("player", word())
+                        .suggests((context, builder) -> {
+                            Faction faction = storage.getFaction(player.getName().getString());
+                            return faction == NULL ? empty() : suggestMatching(faction.getMembers().stream()
+                                    .map(FactionMember::getPlayerName), builder);
+                        })
                         .executes(context -> {
-                            String targetName = StringArgumentType.getString(context, "targetName");
+                            String targetName = getString(context, "player");
                             activityService.clearActivity(targetName);
                             return 1;
                         })
                 );
-    }
-
-    private CompletableFuture<Suggestions> factionMemberSuggestions(com.mojang.brigadier.context.CommandContext<FabricClientCommandSource> context, SuggestionsBuilder builder) {
-        if (player == null) return Suggestions.empty();
-
-        Faction ownFaction = storage.getFaction(player.getName().getString());
-        if (ownFaction != Faction.NULL) {
-            return CommandSource.suggestMatching(
-                    storage.getFactionMembers(ownFaction).stream()
-                            .map(factionMember -> factionMember.getPlayerName()),
-                    builder
-            );
-        }
-
-        return Suggestions.empty();
     }
 }
