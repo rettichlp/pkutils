78e9a0143aafc7c0b3f9da0b89d515de057e35ff
RettichLP <rettichlp01@gmail.com>
2025-08-30 01:15:13 +0200

    feat: enhance FactionListener to implement IMessageReceiveListener for reinforcement messages

--- src/client/java/de/rettichlp/pkutils/listener/impl/faction/FactionListener.java
+++ src/client/java/de/rettichlp/pkutils/listener/impl/faction/FactionListener.java
@@ -3,14 +3,78 @@ package de.rettichlp.pkutils.listener.impl.faction;
 import de.rettichlp.pkutils.common.registry.PKUtilsBase;
 import de.rettichlp.pkutils.common.registry.PKUtilsListener;
 import de.rettichlp.pkutils.common.storage.Storage;
+import de.rettichlp.pkutils.listener.IMessageReceiveListener;
 import de.rettichlp.pkutils.listener.IMessageSendListener;
+import net.minecraft.text.Text;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static de.rettichlp.pkutils.PKUtilsClient.networkHandler;
+import static de.rettichlp.pkutils.PKUtilsClient.player;
 import static de.rettichlp.pkutils.PKUtilsClient.storage;
 import static de.rettichlp.pkutils.common.storage.Storage.ToggledChat.NONE;
+import static java.util.regex.Pattern.compile;
+import static net.minecraft.text.Text.empty;
+import static net.minecraft.text.Text.of;
+import static net.minecraft.util.Formatting.AQUA;
+import static net.minecraft.util.Formatting.BOLD;
+import static net.minecraft.util.Formatting.DARK_AQUA;
+import static net.minecraft.util.Formatting.GRAY;
+import static net.minecraft.util.Formatting.RED;
 
 @PKUtilsListener
-public class FactionListener extends PKUtilsBase implements IMessageSendListener {
+public class FactionListener extends PKUtilsBase implements IMessageReceiveListener, IMessageSendListener {
+
+    private static final Pattern REINFORCEMENT_PATTERN = compile("^(?<type>.+)! (?<sender>.+) benötigt Unterstützung in der Nähe von (?<naviPoint>.+) \\((?<distance>\\d+)m\\)!$");
+    private static final Pattern REINFORCMENT_ON_THE_WAY_PATTERN = compile("^(?<sender>.+) kommt zum Verstärkungsruf von (?<target>.+)! \\((?<distance>\\d+) Meter entfernt\\)$");
+
+    private static final ReinforcementConsumer<String, String, String, String> REINFORCEMENT = (type, sender, naviPoint, distance) -> empty()
+            .append(of(type).copy().formatted(RED, BOLD)).append(" ")
+            .append(of(sender).copy().formatted(AQUA)).append(" ")
+            .append(of("-").copy().formatted(GRAY)).append(" ")
+            .append(of(naviPoint).copy().formatted(AQUA)).append(" ")
+            .append(of("-").copy().formatted(GRAY)).append(" ")
+            .append(of(distance + "m").copy().formatted(DARK_AQUA));
+
+    private static final ReinforcementOnTheWayConsumer<String, String, String> REINFORCEMENT_ON_THE_WAY = (sender, target, distance) -> empty()
+            .append(of("➥").copy().formatted(GRAY)).append(" ")
+            .append(of(sender).copy().formatted(AQUA)).append(" ")
+            .append(of("➡").copy().formatted(GRAY)).append(" ")
+            .append(of(target).copy().formatted(DARK_AQUA)).append(" ")
+            .append(of("- (").copy().formatted(GRAY))
+            .append(of(distance + "m").copy().formatted(DARK_AQUA))
+            .append(of(")").copy().formatted(GRAY));
+
+    @Override
+    public boolean onMessageReceive(String message) {
+        Matcher reinforcementMatcher = REINFORCEMENT_PATTERN.matcher(message);
+        if (reinforcementMatcher.find()) {
+            String type = reinforcementMatcher.group("type");
+            String sender = reinforcementMatcher.group("sender");
+            String naviPoint = reinforcementMatcher.group("naviPoint");
+            String distance = reinforcementMatcher.group("distance");
+
+            Text reinforcement = REINFORCEMENT.create(type, sender, naviPoint, distance);
+            player.sendMessage(reinforcement, false);
+
+            return false;
+        }
+
+        Matcher reinforcementOnTheWayMatcher = REINFORCMENT_ON_THE_WAY_PATTERN.matcher(message);
+        if (reinforcementOnTheWayMatcher.find()) {
+            String sender = reinforcementOnTheWayMatcher.group("sender");
+            String target = reinforcementOnTheWayMatcher.group("target");
+            String distance = reinforcementOnTheWayMatcher.group("distance");
+
+            Text reinforcementAnswer = REINFORCEMENT_ON_THE_WAY.create(sender, target, distance);
+            player.sendMessage(reinforcementAnswer, false);
+
+            return false;
+        }
+
+        return true;
+    }
 
     @Override
     public boolean onMessageSend(String message) {
@@ -21,5 +85,17 @@ public class FactionListener extends PKUtilsBase implements IMessageSendListener
         }
 
         return true;
+    }
+
+    @FunctionalInterface
+    public interface ReinforcementConsumer<Type, Sender, NaviPoint, Distance> {
+
+        Text create(String type, String sender, String naviPoint, String distance);
+    }
+
+    @FunctionalInterface
+    public interface ReinforcementOnTheWayConsumer<Sender, Target, Distance> {
+
+        Text create(String sender, String target, String distance);
     }
 }
